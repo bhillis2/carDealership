938f584b7844624f1af5b71cef38688406821454
Benny Hillis <bhillis2@ivytech.edu>
2017-09-11 07:43:30 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$dbhost = 'localhost';
+$username = 'root';
+$password = '';
+
+      mysqli_connect("$dbhost","$username", "$password");
+
+
+  echo "My first PHP script!    connected2!!!!!!!";
+
+?>
